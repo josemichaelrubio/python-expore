3b7f2a9794efabe2c4de8e6165938e8fe08ecdfe
Jose Michael Rubio <contact@josemichaelrubio.com>
2023-09-13 10:45:27 -0400

    added zoo_list.py

--- days_till_bday.py
+++ days_till_bday.py
@@ -1,3 +1,4 @@
 days_till_birthday = int(input("How many days till your birthday?"))
 weeks_till_birthday = days_till_birthday / 7
 print(f"You have {weeks_till_birthday} weeks till your birthday.")
+
---
+++ zoo_list.py
@@ -0,0 +1,7 @@
+zoo = [ 1,2,3,4,5]
+
+zoo.pop(3)
+zoo.append(9)
+zoo.pop(0)
+print(zoo)
+print(zoo[0:3])
