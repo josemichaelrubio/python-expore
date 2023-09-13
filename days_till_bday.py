e4e18abc25545e84594efa28754384c76af9abe6
Jose Michael Rubio <contact@josemichaelrubio.com>
2023-09-13 09:12:46 -0400

    first commit

---
+++ days_till_bday.py
@@ -0,0 +1,3 @@
+days_till_birthday = int(input("How many days till your birthday?"))
+weeks_till_birthday = days_till_birthday / 7
+print(f"You have {weeks_till_birthday} weeks till your birthday.")
